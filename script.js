0a809dc41a32904ebf4412ee97715c5890001510
Bruno Leonardo <2295132@cegepoutaouais.qc.ca>
2023-02-20 10:06:07 -0600

    version 20/02/2023

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-let aleatoire = Math.ceil(Math.random()*100); //le nombre à deviner
+
+let aleatoire = Math.ceil(Math.random()*100); //le nombre à
+$(function (){
+    nombreChoisi();
+    $("#soumettre").click(nombreChoisi)
+})
+function nombreChoisi () {
+
+    var contenu = $(".nombre").val()
+            if (aleatoire > this) {
+                console.log("Le valeur est plus haut")
+                $(".nombre").attr("disable", true);
+            else
+                if (aleatoire < this) {
+                    console.log("Le valeur est plus bas")
+                    $(".nombre").attr("disable", true)
+                    esle
+                    {
+                        console.log("le nombre est: " + aleatoire);
+                    }
+                }
+
+            }
+    };
+
+
+
+
+
