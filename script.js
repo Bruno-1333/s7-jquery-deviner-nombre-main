cf009a568f48f9e3c906fe7d78503abb957f53b5
Bruno-1333 <2295132@cegepoutaouais.qc.ca>
2023-02-22 15:40:08 -0500

    version finale

--- script.js
+++ script.js
@@ -1,35 +1,102 @@
 
-let aleatoire = Math.ceil(Math.random()*100); //le nombre à
+let aleatoire = Math.ceil(Math.random() * 100 +1); //le nombre à deviner
+let nbEssais = 0; //le nombre d'essais
+let usedNumbers = []; //les nombres déjà utilisés
+let hint = $("#hint"); //le message d'indice
 
-$(function (){
-    soumettre();
-    essai();
-    nouvellePartie();
-})
-function soumettre (){
-    let nombre = $(".nombre").val();
-    if (nombre > aleatoire){
-        $("#reponse").text("C'est moins");
+function addMsgEssais(essaiVal) {
+
+    let msgEssais = $("#msgEssais");
+    if (nbEssais === 1) {
+        msgEssais.removeClass("invisible");
+        //append msgEssaie it adds essai to the end of the list
+        msgEssais.append(essaiVal + " ");
+    } else {
+        msgEssais.append("| " + essaiVal + " ");
     }
-    else if (nombre < aleatoire){
-        $("#reponse").text("C'est plus");
+}
+
+function checkIfUsed(essaiVal) {
+    return usedNumbers.includes(essaiVal);
+}
+
+function checkAnswer(essaiVal) {
+    updateDivs(essaiVal,aleatoire)
+    if (parseInt(essaiVal) === aleatoire) {
+        hint.text("Bravo, vous avez trouvé le nombre mystère en " + nbEssais + " essais !");
+        $(".nombre:not(:contains(" + aleatoire + "))").remove();
+        $(".nombre:contains(" + aleatoire + ")").addClass("numGagnant")
+        $("#new-game-btn").removeClass("invisible")
+        $("#submit").prop("disabled", true);
+
+    } else if (essaiVal < aleatoire) {
+        hint.text("Le nombre à deviner est plus grand!");
+    } else {
+        hint.text("Le nombre à deviner est plus petit!");
     }
-    else{
-        $("#reponse").text("Bravo");
-    }
+}
+
+function disableUsedNumbers() {
+    $(".nombre").each(function () {
+        if (checkIfUsed($(this).text())) {
+            $(this).addClass("used");
+        }
+    });
+}
+function updateDivs(essaiVal, aleatoire) {
+    $(".nombre").each(function() {
+        let number = parseInt($(this).text());
+        if (number > parseInt(essaiVal) && parseInt(essaiVal) > aleatoire) {
+            $(this).remove(); // remove the div if its number is greater than essaiVal and greater than aleatoire
+        } else if (number < parseInt(essaiVal) && parseInt(essaiVal) < aleatoire) {
+            $(this).remove(); // remove the div if its number is smaller than essaiVal and smaller than aleatoire
+        }
+
+    });
 
 }
-function essai (){
-    let nombre = $(".nombre").val();
-    $("#essai").append(nombre + " ");
 
-}
-function nouvellePartie (){
-    aleatoire = Math.ceil(Math.random()*100);
-    $(".nombre").val("");
-    $("#reponse").text("");
-    $("#essai").text("");
-}
+$(function () {
+    console.log(aleatoire)
+    let msgEssais = $("#msgEssais");
+    hint = $("#hint"); // corrected
+
+    msgEssais.addClass("invisible");
+
+    $("button").click(function () {
+
+        let essai = $("#essai");
+        let essaiVal = essai.val();
+        if (checkIfUsed(essaiVal)) {
+            hint.text("Vous avez déjà essayé ce nombre, veuillez en choisir un autre !");
+        } else if (essaiVal < 1 || essaiVal > 100) {
+            hint.text("Veuillez entrer un nombre entre 1 et 100");
+        } else {
+            usedNumbers.push(essaiVal);
+            nbEssais++;
+            if (nbEssais === 10) {
+                addMsgEssais(essaiVal);
+                hint.text("Vous avez atteint le nombre maximal d'essais !");
+                essai.prop("disabled", true);
+                $(this).prop("disabled", true);
+                $("#new-game-btn").removeClass("invisible")
+            } else {
+                addMsgEssais(essaiVal);
+                checkAnswer(essaiVal);
+                disableUsedNumbers();
+            }
+
+        }
+
+
+        essai.val("");
+    });
+
+    $('#new-game-btn').click(function () {
+        location.reload();
+    });
+
+});
 
 
 
